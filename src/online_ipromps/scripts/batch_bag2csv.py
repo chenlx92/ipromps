1397b904b4c7e596956b5a78e4c95f2b16ace189
chenlx92 <chenlx92@outlook.com>
2017-12-04 16:11:41 +0800

    add terminal parm for input directory

--- src/online_ipromps/scripts/batch_bag2csv.py
+++ src/online_ipromps/scripts/batch_bag2csv.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python
-# run it in terminal: rosrun
-# the dir tree is : /dataset/task n/ .rosbag
+# run it in terminal:
+# i.e. python batch_bag2csv.py -d ../datasets/handover_20171128/raw/
+#   or rosrun ipromps batch_bag2csv.py -d ../datasets/handover_20171128/raw/
+
 import glob
 import os
 import subprocess
+from optparse import OptionParser
 
-path = "../datasets/raw/handover_dataset_20171128/wrench"
-all_files = glob.glob(os.path.join(path, "*.bag")) #make list of paths
-print(all_files)
-for file_idx in all_files:
-    subprocess.Popen(['./bag_to_csv.sh ' + file_idx + ' ' +path+'/csv'], shell=True)
 
-print('everyone is happy!!!')
+parser = OptionParser()
+parser.add_option("-d", "--ddbk", action="store",
+                  dest="dataset_dir",
+                  default=None,
+                  help="the directory of dataset")
+(options, args) = parser.parse_args()
+path = options.dataset_dir
+
+
+if path is None:
+    print('please input the dataset directory.')
+
+else:
+    task_list = glob.glob(os.path.join(path, "*"))
+    for task_list_idx in task_list:
+        file_list = glob.glob(os.path.join(task_list_idx, "*.bag"))
+        if file_list == []:
+            print('No rosbag found. ')
+            continue
+        for file_idx in file_list:
+            subprocess.Popen(['./bag_to_csv.sh ' + file_idx + ' ' + task_list_idx + '/csv'], shell=True)
