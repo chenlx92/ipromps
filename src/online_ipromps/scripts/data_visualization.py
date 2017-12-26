7d54c6c52206c2687838011141a806cfd000c79e
chenlx92 <chenlx92@outlook.com>
2017-12-26 15:14:43 +0800

    trival changes

--- src/online_ipromps/scripts/data_visualization.py
+++ src/online_ipromps/scripts/data_visualization.py
@@ -17,21 +17,21 @@ ipromps_set_post = joblib.load(path + '/ipromps_set_post.pkl')
 datasets_len101 = joblib.load(path + '/datasets_len101.pkl')
 robot_traj = joblib.load(path + '/robot_traj.pkl')
 
-# plot the predict robot motion
-fig = plt.figure(200)
-for joint_idx in range(7):
-    ax = fig.add_subplot(7, 1, 1 + joint_idx)
-    plt.plot(np.linspace(0, 1.0, 101), robot_traj[:, joint_idx])
+# # plot the predict robot motion
+# fig = plt.figure(200)
+# for joint_idx in range(7):
+#     ax = fig.add_subplot(7, 1, 1 + joint_idx)
+#     plt.plot(np.linspace(0, 1.0, 101), robot_traj[:, joint_idx])
+#
+# # plot the norm data
+# for task_idx, ipromps_idx in enumerate(datasets_len101):
+#     fig = plt.figure(task_idx+300)
+#     for demo_idx in range(20):
+#         for joint_idx in range(7):
+#             ax = fig.add_subplot(7, 1, 1 + joint_idx)
+#             plt.plot(np.linspace(0, 1.0, 101), datasets_len101[task_idx][demo_idx]['left_joints'][:, joint_idx])
 
-# plot the norm data
-for task_idx, ipromps_idx in enumerate(datasets_len101):
-    fig = plt.figure(task_idx+300)
-    for demo_idx in range(20):
-        for joint_idx in range(7):
-            ax = fig.add_subplot(7, 1, 1 + joint_idx)
-            plt.plot(np.linspace(0, 1.0, 101), datasets_len101[task_idx][demo_idx]['left_joints'][:, joint_idx])
-
-
+# robot joint
 # # plot datasets4train_post data
 # for task_idx, ipromps_idx in enumerate(datasets4train_post):
 #     fig = plt.figure(task_idx)
@@ -53,6 +53,29 @@ for task_idx, ipromps_idx in enumerate(datasets_len101):
 #     for joint_idx in range(7):
 #         ax = fig.add_subplot(7, 1, 1 + joint_idx)
 #         ipromps_idx.promps[joint_idx+11].plot_nUpdated(color='r', via_show=False)
+
+# emg
+# plot datasets4train_post data
+for task_idx, ipromps_idx in enumerate(datasets4train_post):
+    fig = plt.figure(task_idx)
+    for demo_idx in range(20):
+        for joint_idx in range(8):
+            ax = fig.add_subplot(8, 1, 1 + joint_idx)
+            plt.plot(np.linspace(0, 1.0, 101), datasets4train_post[task_idx][demo_idx]['emg'][:, joint_idx])
+# plot the prior
+for task_idx, ipromps_idx in enumerate(ipromps_set_post):
+    fig = plt.figure(task_idx)
+    fig.suptitle('emg')
+    for joint_idx in range(8):
+        ax = fig.add_subplot(8, 1, 1+joint_idx)
+        ipromps_idx.promps[joint_idx].plot_prior()
+# plot post distribution
+for task_idx, ipromps_idx in enumerate(ipromps_set_post):
+    print(ipromps_idx.promps[0].alpha_fit)
+    fig = plt.figure(task_idx)
+    for joint_idx in range(8):
+        ax = fig.add_subplot(8, 1, 1 + joint_idx)
+        ipromps_idx.promps[joint_idx].plot_nUpdated(color='r', via_show=True)
 
 # # plot the norm data
 # for task_idx, ipromps_idx in enumerate(datasets_len101):
