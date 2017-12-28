4732a124d27f31e6562bb42b4f6af431810bed05
chenlx92 <chenlx92@outlook.com>
2017-12-28 11:16:00 +0800

    filter the data before construct a full data array, only this order make sense

--- src/online_ipromps/scripts/train_offline.py
+++ src/online_ipromps/scripts/train_offline.py
@@ -17,23 +17,24 @@ datasets_norm_dir = '../datasets/handover_20171128/pkl/datasets_len101.pkl'
 # datasets param
 num_joints = 8+3+7
 num_obs_joints = 8+3
-num_demos = 20
+num_demos = 10
+len_norm = 101
 
 # optional ipromps model param
-len_norm = 101
 num_basis = 31
 sigma_basis = 0.05
 num_alpha_candidate = 10
 
 # measurement noise
-emg_noise = 0.3
-hand_noise = 0.3
-pose_noise = 0.3
-measure_noise_cov_full = scipy.linalg.block_diag(np.eye(8) * emg_noise,
-                                                 np.eye(3) * hand_noise,
-                                                 np.eye(7) * pose_noise)
+emg_noise = 500
+hand_noise = 0.05
+joints_noise = 0.05
+noise_cov_full = scipy.linalg.block_diag(np.eye(8) * emg_noise,
+                                         np.eye(3) * hand_noise,
+                                         np.eye(7) * joints_noise)
+
 # the med filter kernel
-filt_kernel = [9, 1]
+filt_kernel = [13, 1]
 
 ###########################
 
@@ -47,13 +48,14 @@ y_full = np.array([]).reshape(0, num_joints)
 for datasets4train_idx in datasets4train:
     for demo_idx in datasets4train_idx:
         h = np.hstack([demo_idx['emg'], demo_idx['left_hand'], demo_idx['left_joints']])
+        h = signal.medfilt(h, filt_kernel)  # filter the norm data
         y_full = np.vstack([y_full, h])
 min_max_scaler = preprocessing.MinMaxScaler()
 datasets_norm_full = min_max_scaler.fit_transform(y_full)
 
-# a filter
-print('Filtering the data...')
-datasets_norm_full = signal.medfilt(datasets_norm_full, filt_kernel)
+# # a filter: is wrong actually here
+# print('Filtering the data...')
+# datasets_norm_full = signal.medfilt(datasets_norm_full, filt_kernel)
 
 # construct a data structure to train the model
 datasets4train_post = []
@@ -69,9 +71,10 @@ for task_idx in range(len(datasets4train)):
     datasets4train_post.append(datasets_temp)
 
 # create iProMPs sets
-ipromps_set = [ipromps_lib.IProMP(num_joints=num_joints, num_basis=num_basis, sigma_basis=sigma_basis,
-                                  num_samples=len_norm, num_obs_joints=num_obs_joints,
-                                  sigmay=measure_noise_cov_full) for x in datasets4train]
+ipromps_set = [ipromps_lib.IProMP(num_joints=num_joints, num_obs_joints=num_obs_joints, num_basis=num_basis,
+                                  sigma_basis=sigma_basis, num_samples=len_norm, sigmay=noise_cov_full,
+                                  min_max_scaler=min_max_scaler, num_alpha_candidate=num_alpha_candidate)
+               for x in datasets4train]
 
 # add demo and alpha var for each IProMPs
 for idx, task_idx in enumerate(ipromps_set):
@@ -84,8 +87,6 @@ for idx, task_idx in enumerate(ipromps_set):
 
 # save the trained models
 print('Saving the trained models...')
-joblib.dump([ipromps_set, datasets4train_post, min_max_scaler, filt_kernel], datasets_path + '/ipromps_set.pkl')
+joblib.dump([ipromps_set, datasets4train_post, filt_kernel], datasets_path + '/ipromps_set.pkl')
 
 print('Trained the IProMPs successfully!!!')
-
-
