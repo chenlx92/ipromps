30421f1511ebca9f3033dfcc0aae5d6e106600fc
chenlx92 <chenlx92@outlook.com>
2018-01-10 21:31:03 +0800

    the script including loading data and training models

---
+++ src/online_ipromps/scripts/train_offline.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import load_data
+import train_models
+
+
+def main():
+    print('Running the %s' % load_data.__name__)
+    load_data.main()
+    print('Running the %s' % train_models.__name__)
+    train_models.main()
+
+if __name__ == '__main__':
+    main()
