3859ffecaed9158a78ccd854225af200543b4b4f
chenlx92 <chenlx92@outlook.com>
2018-01-19 17:04:14 +0800

    purge the package

--- src/discernment_task/show_results.py
+++
@@ -1,9 +0,0 @@
-import glob
-import os
-import sys
-import subprocess
-
-path = "./"
-dataset_num = 30
-for i in range(dataset_num):
-    subprocess.Popen(['python imu_emg_pose_test_compact.py -t %s' %str(i)], shell = True)
