5a113da217238d3e9591c382bbc2268f59305fd3
chenlx92 <chenlx92@outlook.com>
2018-03-29 22:23:00 +0800

    chinese char supported

--- scripts/visualization.py
+++ scripts/visualization.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# coding:utf-8
 from sklearn.externals import joblib
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,6 +8,7 @@ import scipy.stats as stats
 import os
 import ConfigParser
 from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.font_manager import FontProperties
 
 
 # read conf file
@@ -18,14 +20,15 @@ datasets_path = os.path.join(file_path, cp_models.get('datasets', 'path'))
 
 # load datasets
 ipromps_set = joblib.load(os.path.join(datasets_path, 'pkl/ipromps_set.pkl'))
-ipromps_set_post = joblib.load(os.path.join(datasets_path, 'pkl/ipromps_set_post.pkl'))
+# ipromps_set_post = joblib.load(os.path.join(datasets_path, 'pkl/ipromps_set_post.pkl'))
 datasets_norm_preproc = joblib.load(os.path.join(datasets_path, 'pkl/datasets_norm_preproc.pkl'))
+datasets_norm = joblib.load(os.path.join(datasets_path, 'pkl/datasets_norm.pkl'))
 datasets_raw = joblib.load(os.path.join(datasets_path, 'pkl/datasets_raw.pkl'))
 datasets_filtered = joblib.load(os.path.join(datasets_path, 'pkl/datasets_filtered.pkl'))
 task_name = joblib.load(os.path.join(datasets_path, 'pkl/task_name_list.pkl'))
-[robot_traj_offline, ground_truth] = joblib.load(os.path.join(datasets_path, 'pkl/robot_traj_offline.pkl'))
-robot_traj_online = joblib.load(os.path.join(datasets_path, 'pkl/robot_traj_online.pkl'))
-obs_data_online = joblib.load(os.path.join(datasets_path, 'pkl/obs_data_online.pkl'))
+# [robot_traj_offline, ground_truth] = joblib.load(os.path.join(datasets_path, 'pkl/robot_traj_offline.pkl'))
+# robot_traj_online = joblib.load(os.path.join(datasets_path, 'pkl/robot_traj_online.pkl'))
+# obs_data_online = joblib.load(os.path.join(datasets_path, 'pkl/obs_data_online.pkl'))
 
 
 
@@ -47,6 +50,13 @@ joint_num = info_n_idx[info][1] - info_n_idx[info][0]
 num_obs = cp_models.getint('visualization', 'num_obs')
 
 
+# zh config
+def conf_zh(font_name):
+    from pylab import mpl
+    mpl.rcParams['font.sans-serif'] = [font_name]
+    mpl.rcParams['axes.unicode_minus'] = False
+
+
 # plot the raw data
 def plot_raw_data(num=0):
     for task_idx, ipromps_idx in enumerate(ipromps_set):
@@ -56,8 +66,22 @@ def plot_raw_data(num=0):
             for joint_idx in range(joint_num):
                 ax = fig.add_subplot(joint_num, 1, 1 + joint_idx)
                 data = datasets_raw[task_idx][demo_idx][info][:, joint_idx]
-                plt.plot(range(len(data)), data)
+                plt.plot(np.array(range(len(data)))/100.0, data)
+                ax.set_xlabel(u'时间/秒')
+                ax.set_ylabel(u'位移/米')
 
+# plot the norm data
+def plot_norm_data(num=0):
+    for task_idx, ipromps_idx in enumerate(ipromps_set):
+        fig = plt.figure(task_idx + num)
+        fig.suptitle('the raw data of ' + info)
+        for demo_idx in range(ipromps_idx.num_demos):
+            for joint_idx in range(joint_num):
+                ax = fig.add_subplot(joint_num, 1, 1 + joint_idx)
+                data = datasets_norm[task_idx][demo_idx][info][:, joint_idx]
+                plt.plot(np.array(range(len(data)))/100.0, data)
+                ax.set_xlabel(u'时间/秒')
+                ax.set_ylabel(u'位移/米')
 
 # plot the filtered data
 def plot_filtered_data(num=0):
@@ -68,7 +92,10 @@ def plot_filtered_data(num=0):
             for joint_idx in range(joint_num):
                 ax = fig.add_subplot(joint_num, 1, 1 + joint_idx)
                 data = datasets_filtered[task_idx][demo_idx][info][:, joint_idx]
-                plt.plot(range(len(data)), data)
+                plt.plot(np.array(range(len(data)))/100.0, data, )
+                ax.set_xlabel(u'时间/秒')
+                ax.set_ylabel(u'位移/米')
+                plt.legend()
 
 
 # plot the prior distribution
@@ -93,10 +120,10 @@ def plot_alpha(num=0):
         pdf = stats.norm.pdf(h, h_mean, h_std)
         pl.hist(h, normed=True, color='b')
         plt.plot(h, pdf, linewidth=5, color='r', marker='o', markersize=10)
-        candidate = ipromp.alpha_candidate()
-        candidate_x = [x['candidate'] for x in candidate]
-        prob = [x['prob'] for x in candidate]
-        plt.plot(candidate_x, prob, linewidth=0, color='g', marker='o', markersize=14)
+        # candidate = ipromp.alpha_candidate()
+        # candidate_x = [x['candidate'] for x in candidate]
+        # prob = [x['prob'] for x in candidate]
+        # plt.plot(candidate_x, prob, linewidth=0, color='g', marker='o', markersize=14)
 
 
 # plot the post distribution
@@ -165,8 +192,9 @@ def plot_3d_filtered_h_traj(num=0):
         ax = fig.gca(projection='3d')
         for demo_idx in demo_list:
             data = datasets_filtered[task_idx][demo_idx]['left_hand']
-            ax.plot(data[:, 0], data[:, 1], data[:, 2], linewidth=3,
-                    label='training sets about human '+str(demo_idx), alpha=0.5)
+            ax.plot(data[:, 0], data[:, 1], data[:, 2], linewidth=2,
+                    # label='training sets about human '+str(demo_idx), alpha=0.3)
+                    alpha=1.0)
             ax.set_xlabel('X Label')
             ax.set_ylabel('Y Label')
             ax.set_zlabel('Z Label')
@@ -181,12 +209,14 @@ def plot_offline_3d_obs(num=0):
         obs_data_dict = ground_truth
         data = obs_data_dict['left_hand']
         ax.plot(data[0:num_obs, 0], data[0:num_obs, 1], data[0:num_obs, 2],
-                'o', linewidth=3, label='obs points', alpha=0.5)
+                # 'o', markersize=10, label='observation points', alpha=1.0, color='green')
+                'o', markersize = 12, label = u'人运动的观测点', alpha = 1.0, color = 'green')
         ax.plot(data[:, 0], data[:, 1], data[:, 2],
-                '-', linewidth=8, color='r', label='obs points', alpha=0.3)
-        ax.set_xlabel('X Label')
-        ax.set_ylabel('Y Label')
-        ax.set_zlabel('Z Label')
+                # '-', linewidth=5, color='blue', label='ground truth human trajectory', alpha=1.0)
+                '-', linewidth = 5, color = 'blue', label = u'人运动轨迹的真实值', alpha = 1.0)
+        # ax.set_xlabel('X Label')
+        # ax.set_ylabel('Y Label')
+        # ax.set_zlabel('Z Label')
         ax.legend()
 
 
@@ -198,10 +228,14 @@ def plot_3d_filtered_r_traj(num=0):
         for demo_idx in demo_list:
             data = datasets_filtered[task_idx][demo_idx]['left_joints']
             ax.plot(data[:, 0], data[:, 1], data[:, 2],
-                    linewidth=3, linestyle='-', label='training sets about robot '+str(demo_idx), alpha=0.5)
-            ax.set_xlabel('X Label')
-            ax.set_ylabel('Y Label')
-            ax.set_zlabel('Z Label')
+                    # linewidth=3, linestyle='-', label='training sets about robot '+str(demo_idx), alpha=0.3)
+                    linewidth=2, linestyle='-', alpha=1.0)
+            # ax.set_xlabel('X Label')
+            # ax.set_ylabel('Y Label')
+            # ax.set_zlabel('Z Label')
+            ax.set_xlabel(u'X/米')
+            ax.set_ylabel(u'Y/米')
+            ax.set_zlabel(u'Z/米')
             ax.legend()
 
 
@@ -212,13 +246,15 @@ def plot_gen_3d_offline_r_traj(num=0):
         ax = fig.gca(projection='3d')
         data = robot_traj_offline
         ax.plot(data[:, 0], data[:, 1], data[:, 2],
-                linewidth=8, linestyle='-', label='generated robot traj')
+                # linewidth=5, linestyle='--', label='generated robot trajectory')
+                linewidth = 5, linestyle = '--', label = u'生成的机器人运动轨迹')
         data = ground_truth['left_joints']
         ax.plot(data[:, 0], data[:, 1], data[:, 2],
-                linewidth=8, linestyle='-', color='r', label='ground truth robot traj', alpha=0.3)
-        ax.set_xlabel('X Label')
-        ax.set_ylabel('Y Label')
-        ax.set_zlabel('Z Label')
+                # linewidth=5, linestyle='-', color='r', label='ground truth robot trajectory', alpha=1.0)
+                linewidth=5, linestyle='-', color='r', label=u'机器人运动轨迹的真实值', alpha=1.0)
+        ax.set_xlabel(u'X/米')
+        ax.set_ylabel(u'Y/米')
+        ax.set_zlabel(u'Z/米')
         ax.legend()
 
 
@@ -249,14 +285,12 @@ def plot_online_3d_obs(num):
         ax.legend()
 
 
-
-
 # plot offline test pair
 def pairs_offline(num=0):
     plot_3d_filtered_h_traj(num)
     plot_3d_filtered_r_traj(num)
-    plot_offline_3d_obs(num)
-    plot_gen_3d_offline_r_traj(num)
+    # plot_offline_3d_obs(num)
+    # plot_gen_3d_offline_r_traj(num)
 
 
 # plot online test pair
@@ -268,10 +302,12 @@ def pairs_online(num=0):
 
 
 def main():
+    conf_zh("Droid Sans Fallback")
     # plt.close('all')
-    # plot_raw_data()
-    # plot_filtered_data()
-    # plot_prior()
+    # plot_raw_data(0)
+    # plot_norm_data(2)
+    plot_filtered_data(1)
+    # plot_prior(1)
     # plot_post()
     # plot_alpha()
     # plot_robot_traj()
@@ -281,9 +317,9 @@ def main():
     #3D
     # plot_3d_raw_traj(10)
     # plot_3d_gen_r_traj_online(10)
-    # pairs_offline(20)
-    pairs_online(10)
-
+    # pairs_offline(10)
+    # pairs_online(10)
+    # plt.legend(prop = {'size': 20})
     plt.show()
 
 
